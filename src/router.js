c0d958c1ba785e0eca1c8488f386ec3eed9f6ef4
RZMAYMAN27 <rizaumamiayman10@gmail.com>
2024-06-26 13:01:03 +0700

    vue router

--- src/router.js
+++ src/router.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import TaskPage from './components/TaskPage.vue'; 
-import HomePage from './components/HomePage.vue'; 
+import HomePage from './components/HomePage.vue';
+import TugasPertemuan1 from './components/TugasPertemuan1.vue';
+import TugasPertemuan2 from './components/TugasPertemuan2.vue';
+import TugasPertemuan3 from './components/TugasPertemuan3.vue';
+import TugasPertemuan4 from './components/TugasPertemuan4.vue';
+import TugasPertemuan5 from './components/TugasPertemuan5.vue';
+import TugasPertemuan6 from './components/TugasPertemuan6.vue';
+import TugasPertemuan7 from './components/TugasPertemuan7.vue';
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/task/:id', component: TaskPage }
+  { path: '/task/1', component: TugasPertemuan1 },
+  { path: '/task/2', component: TugasPertemuan2 },
+  { path: '/task/3', component: TugasPertemuan3 },
+  { path: '/task/4', component: TugasPertemuan4 },
+  { path: '/task/5', component: TugasPertemuan5 },
+  { path: '/task/6', component: TugasPertemuan6 },
+  { path: '/task/7', component: TugasPertemuan7 }
 ];
 
 const router = createRouter({
